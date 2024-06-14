30cc58ee0f98ea950e39c8e54185f26f9675d66e
kiieeaa <kiyaadinda5@gmail.com>
2024-06-14 10:01:52 +0700

    make the right

--- BinerySearchTree_0119.cpp
+++ BinerySearchTree_0119.cpp
@@ -27,4 +27,6 @@ public:
 		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memmory for the new node
 		newNode->info = element;  // Assign to the data field of the new node
 		newNode->leftchild = nullptr;  //Make the left child of the new node point to null
+		newNode->rightchild = nullptr; // Make the right child of the new node point to null
+
 
