945b517332f78ebb78ad201c42e3027b57f4db16
kiieeaa <kiyaadinda5@gmail.com>
2024-06-14 10:04:47 +0700

    this function searches the current

--- BinerySearchTree_0119.cpp
+++ BinerySearchTree_0119.cpp
@@ -41,5 +41,11 @@ public:
 		if (element < parent->info) {  // If the value in the data field of the new node is less than the value in the data field of the parent node
 			parent->leftchild = newNode;  // Make the left child of the parent point to the new node
 		}
+		else if (element > parent->info) {  // If the value in the data field of the new node is greater than the value in the data field of the parent node
+			parent->rightchild = newNode;  // Make the right child of the parent point to the new node
+		}
+	}
+	void search(string element, Node*& parent, Node*& currentNode) {  // Locate the node which will be the parent of the new node
+		// This function searches the current Node of the specified Node as well as the currentNode of its parent
 
 
