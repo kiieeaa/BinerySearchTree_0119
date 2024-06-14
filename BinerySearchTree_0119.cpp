bed5ab539de6b73c1f071a074ddba9442109f628
kiieeaa <kiyaadinda5@gmail.com>
2024-06-14 10:11:38 +0700

    inorder

--- BinerySearchTree_0119.cpp
+++ BinerySearchTree_0119.cpp
@@ -127,3 +127,28 @@ int main()
 			x.insert(word);
 			break;
 		}
+		case '2':
+		{
+			x.inorder(x.ROOT);
+			break;
+		}
+		case '3':
+		{
+			x.preorder(x.ROOT);
+			break;
+		}
+		case '4':
+		{
+			x.postorder(x.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
+}
