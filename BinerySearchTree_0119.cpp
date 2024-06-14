a02552db6718a1897e1590b436e2b4f1e4d143aa
kiieeaa <kiyaadinda5@gmail.com>
2024-06-14 10:03:36 +0700

    if the value

--- BinerySearchTree_0119.cpp
+++ BinerySearchTree_0119.cpp
@@ -29,4 +29,17 @@ public:
 		newNode->leftchild = nullptr;  //Make the left child of the new node point to null
 		newNode->rightchild = nullptr; // Make the right child of the new node point to null
 
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+
+		search(element, parent, currentNode);   // Locate the node which will be the parent of the new node
+		if (parent == nullptr) {  // If the parent is NULL (Tree is empty)
+			ROOT = newNode;  // mark the new node as the root
+			return;  // exit
+		}
+
+		if (element < parent->info) {  // If the value in the data field of the new node is less than the value in the data field of the parent node
+			parent->leftchild = newNode;  // Make the left child of the parent point to the new node
+		}
+
 
